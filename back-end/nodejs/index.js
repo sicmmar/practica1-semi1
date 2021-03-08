05aa74ebaa4ed0a1a087ed37f7cb9826b42fefdf
Elba Alvarez <elba1995@gmail.com>
2021-03-07 18:41:06 -0600

    modificando rutas

--- back-end/nodejs/index.js
+++ back-end/nodejs/index.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 const mysql = require('mysql');
 const myConnection = require('express-myconnection');
 var uuid = require('uuid');
-//const aws_keys = require('./creds');
+const aws_keys = require('./creds');
 var corsOptions = { origin: true, optionsSuccessStatus: 200 };
 app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
-//const db_credentials = require('./db_creds');
-//app.use(myConnection(mysql, db_credentials, 'single'));
+const db_credentials = require('./db_creds');
+app.use(myConnection(mysql, db_credentials, 'single'));
 
 
 //instanciamos el sdk
@@ -42,11 +42,11 @@ app.get('/', (req, res) => {
 //Ingreso a la app
 app.post('/ingresar', (req, res) => {
 
-  var usu = req.json.get('username');
-  var contra = req.json.get('contrasena');
+  var usu = req.body.username;
+  var contra = req.body.contrasena;
 
   if (!(usu || contra)) {
-    return jsonify({ 'status': 404, 'Item': '' });
+    return JSON.stringify({ 'status': 404, 'Item': '' });
   }
 
   //Encontrar Usuario
@@ -59,32 +59,34 @@ app.post('/ingresar', (req, res) => {
 
   var item = ddb.getItem(params);
   if (item) {
-    if (item['contrasena']['S'] == contra) {
+    console.log('Existe usuario');
+    if (item.contrasena == contra) {
       console.log("Contrasena correcta");
-      return jsonify({ 'status': 202, 'Item': item })
+      return JSON.stringify({ 'status': 202, 'Item': '' });
     } else {
 
       console.log("Contrasena incorrecta");
-      return jsonify({ 'status': 202, 'Item': '' })
+      return JSON.stringify({ 'status': 202, 'Item': '' });
     }
   } else {
-    return jsonify({ 'status': 303, 'Item': "" })
+    console.log('No existe usuario');
+    return JSON.stringify({ 'status': 303, 'Item': "" });
   }
 
 });
 
 //Registro de nuevo usuario
 app.post('/registrar', (req, res) => {
-  usu = req.json.get('username');
-  nombre = req.json.get('nombre');
-  contra = req.json.get('contrasena');
-  nFoto = req.json.get('nFoto');
-  ext = req.json.get('ext');
-  b64 = req.json.get('b64');
-  uniqueID = str(uuid.uuid1().time_low);
+  var usu = req.body.username;
+  var nombre = req.body.nombre;
+  var contra = req.body.contrasena;
+  var nFoto = req.body.nFoto;
+  var ext = req.body.ext;
+  var b64 = req.body.b64;
+  var uniqueID = `${nFoto}-${uuid()}.${ext}`;;
 
   if (!(usu || nombre || contra || nFoto)) {
-    return jsonify({ 'status': 504, 'existe': '' });
+    return JSON.stringify({ 'status': 504, 'existe': '' });
   }
 
   //Encontrar Usuario
@@ -96,7 +98,7 @@ app.post('/registrar', (req, res) => {
   };
 
   var item = ddb.getItem(params);
-  if (item) { return jsonify({ 'status': 404, 'existe': 'true' }) }
+  if (item) { console.log('Existe usuario ya'); return JSON.stringify({ 'status': 404, 'existe': 'true' }); }
   else {
     var nombrei = "fotos_perfil/" + nFoto + "-" + uniqueID + "." + ext;
     //Se convierte la base64 a bytes
@@ -124,9 +126,12 @@ app.post('/registrar', (req, res) => {
       }
     );
     var status = resp['ResponseMetadata']['HTTPStatusCode'];
-    return jsonify({ 'status': status, 'existe': "false" });
+    console.log('Registro');
+    return JSON.stringify({ 'status': status, 'existe': "false" });
 
   }
 });
+
+
 
 
