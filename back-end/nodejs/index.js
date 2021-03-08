e3dd6d58d5adfe21b3b2b561c2aa90abb021ce5b
Elba Alvarez <elba1995@gmail.com>
2021-03-07 23:21:05 -0600

    añadiendo rutas edicion y nuevo album

--- back-end/nodejs/index.js
+++ back-end/nodejs/index.js
@@ -37,6 +37,7 @@ console.log('Listening on port', port);
 app.get('/', (req, res) => {
   console.log('Hola mundo');
   res.send('Hola mundo');
+  return JSON.stringify({ 'status': 200, 'Item': '' });
 });
 
 //Ingreso a la app
@@ -83,7 +84,7 @@ app.post('/registrar', (req, res) => {
   var nFoto = req.body.nFoto;
   var ext = req.body.ext;
   var b64 = req.body.b64;
-  var uniqueID = `${nFoto}-${uuid()}.${ext}`;;
+  var uniqueID = `${nFoto}-${uuid()}.${ext}`;
 
   if (!(usu || nombre || contra || nFoto)) {
     return JSON.stringify({ 'status': 504, 'existe': '' });
@@ -115,23 +116,141 @@ app.post('/registrar', (req, res) => {
     const putResult = s3.putObject(params).promise();
     res.json({ mensaje: putResult });
 
-    var resp = dynamo.put_item(
+    dynamo.put_item(
       TableName = 'usuario',
       Item = {
         'username': { 'S': usu },
         'nombre': { 'S': nombre },
         'contrasena': { 'S': contra },
         'nFoto': { 'S': nFoto + '.' + ext },
-        'foto_perfil': { 'S': "https://practica1-g45-imagenes.s3.us-east-2.amazonaws.com/" + nombrei }
+        'foto_perfil': { 'S': "https://practica1-g45-imagenes.s3.us-east-2.amazonaws.com/" + nombrei },
+        'album': { 'L': [{ 'L': [{ 'S': 'Perfil' }, { 'L': [{ 'S': "https://practica1-g45-imagenes.s3.us-east-2.amazonaws.com/" + nombrei }] }] }] }
       }
     );
-    var status = resp['ResponseMetadata']['HTTPStatusCode'];
     console.log('Registro');
-    return JSON.stringify({ 'status': status, 'existe': "false" });
+    return JSON.stringify({ 'status': 202, 'existe': "false" });
 
   }
 });
 
 
+//Editar Perfil
+app.post('/editarPerfil', (req, res) => {
+  var usu = req.body.username;
+  var nombre = req.body.nombre;
+  var contra = req.body.contrasena;
+  var nFoto = req.body.nFoto;
+  var ext = req.body.ext;
+  var b64 = req.body.b64;
+  var uniqueID = `${nFoto}-${uuid()}.${ext}`;
 
+  if (!(usu || nombre || contra || nFoto)) {
+    return JSON.stringify({ 'status': 504, 'existe': '' });
+  }
+
+  //Encontrar Usuario
+  var params = {
+    TableName: 'usuario',
+    Key: {
+      'username': { 'S': usu }
+    }
+  };
+
+  var item = ddb.getItem(params);
+  if (item) {
+    console.log('Existe usuario ya');
+    if (item.contrasena == contra) {
+      console.log("Contrasena correcta");
+      var nombrei = "fotos_perfil/" + nFoto + "-" + uniqueID + "." + ext;
+      //Se convierte la base64 a bytes
+      let buff = new Buffer.from(b64, 'base64');
+
+      var album_perfil = item.album;
+      for (var alb in album_perfil['L']) {
+        if (alb['L'][0]['S'] == 'Perfil') {
+          alb['L'][1]['L'].append({ 'S': "https://practica1-g45-imagenes.s3.us-east-2.amazonaws.com/" + nombrei });
+          break;
+        }
+      }
+      const params = {
+        Bucket: bucketname,
+        Key: nombrei,
+        Body: buff,
+        ContentType: "image",
+        ACL: 'public-read'
+      };
+
+      const putResult = s3.putObject(params).promise();
+      res.json({ mensaje: putResult });
+
+      dynamo.update_item(
+        TableName = 'usuario',
+        Item = {
+          'username': { 'S': usu },
+          'nombre': { 'S': nombre },
+          'contrasena': { 'S': contra },
+          'nFoto': { 'S': nFoto + '.' + ext },
+          'foto_perfil': { 'S': "https://practica1-g45-imagenes.s3.us-east-2.amazonaws.com/" + nombrei },
+          'album': { 'L': [{ 'L': [{ 'S': 'Perfil' }, { 'L': [{ 'S': "https://practica1-g45-imagenes.s3.us-east-2.amazonaws.com/" + nombrei }] }] }] }
+        },
+        UpdateExpression = 'set nombre=:n, nFoto=:nf, foto_perfil=:fp, album=:prof',
+        ExpressionAttributeValues = {
+          ':n': { 'S': nombre },
+          ':nf': { 'S': nFoto + '.' + ext },
+          ':fp': { 'S': "https://practica1-g45-imagenes.s3.us-east-2.amazonaws.com/" + nombrei },
+          ':prof': album_perfil
+        }
+      );
+      console.log('Actualizado');
+      return JSON.stringify({ 'status': 202, 'Item': "" })
+    }
+    else {
+      return JSON.stringify({ 'status': 303, 'Item': ""})
+    }
+  }
+
+  return JSON.stringify({ 'status': 202, 'Item': '' })
+
+});
+
+app.post('/newAlbum', (req, res) => {
+  var usern = req.body.username;
+  var nombre_album = req.body.album;
+
+  if (!(usern || nombre_album)) {
+    return JSON.stringify({ 'status': 504, 'existe': '' });
+  }
+
+  //Encontrar Usuario
+  var params = {
+    TableName: 'usuario',
+    Key: {
+      'username': { 'S': usern }
+    }
+  };
+
+  var item = ddb.getItem(params);
+  if (item) {
+    console.log('Existe usuario ya');
+    var albumes = item.album;
+    albumes['L'].append({ 'L': [{ 'S': nombre_album }, { 'L': [] }] });
+
+    dynamo.update_item(
+      TableName = 'usuario',
+      Key = {
+        'username': { 'S': usern }
+      },
+      UpdateExpression = 'set album=:nal',
+      ExpressionAttributeValues = {
+        ':nal': albumes
+      }
+    );
+    console.log('Actualizado');
+    return JSON.stringify({ 'status': 202, 'existe': usuarioExistente(usern) });
+  }
+  else {
+    console.log('No actualizado');
+    return jsonify({'status': 404,'existe':'true'});
+  }
+});
 
