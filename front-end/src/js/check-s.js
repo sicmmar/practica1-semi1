b3cfa32c02645bef1fd82ed8a5ddfbec0569799c
Mariana Sic <sicmariana8@gmail.com>
2021-03-07 21:15:35 -0600

    endpoint album

--- front-end/src/js/check-s.js
+++
@@ -1,7 +0,0 @@
-function verificarToken(){
-    const pass = sessionStorage.getItem('contrasena');
-    if (pass == null){
-        window.location = 'login.html';
-    }
-    console.log("***************** " + pass)
-}
